a0f65e7bbcb4492eea4cf2ac67611cdd38931939
Tom Chen <developer@tomchentw.com>
2016-02-21 12:23:35 +0800

    feat(index.js): add ca

--- src/index.js
+++ src/index.js
@@ -15,6 +15,8 @@ import {
   default as app,
 } from "./app";
 
+const ENCODING = `utf8`;
+
 function getSecretPathname() {
   if (process.env.NODE_ENV === `production`) {
     return `/opt/secret/production`;
@@ -23,18 +25,31 @@ function getSecretPathname() {
   }
 }
 
+function getCA() {
+  // http://stackoverflow.com/a/20444809/1458162
+  if (process.env.NODE_ENV === `production`) {
+    return Promise.all([
+      fs.readFile(`/opt/secret/letsencrypt.intermediate.pem`, ENCODING),
+    ]);
+  } else {
+    return [];
+  }
+}
+
 async function startServer() {
   const pathname = getSecretPathname();
   // https://devcenter.heroku.com/articles/ssl-certificate-self
   // http://blog.getpostman.com/2014/01/28/using-self-signed-certificates-with-postman/
-  const [key, cert] = await Promise.all([
-    fs.readFile(`${ pathname }.key`),
-    fs.readFile(`${ pathname }.crt`),
+  const [key, cert, ca] = await Promise.all([
+    fs.readFile(`${ pathname }.key`, ENCODING),
+    fs.readFile(`${ pathname }.crt`, ENCODING),
+    getCA(),
   ]);
 
   const server = http2.createServer({
     key,
     cert,
+    ca,
   }, app.callback());
 
   return thenify(::server.listen)(3000);
