0663c1ba8817f11a151bbf7de83d3c8e4de01871
Tom Chen <developer@tomchentw.com>
2016-02-21 10:55:38 +0800

    feat(docker): extract docker related to environment variables

--- src/docker/index.js
+++ src/docker/index.js
@@ -13,7 +13,7 @@ export const run = thenify(::docker.run);
 export default docker;
 
 export const runBuild = (dirpath) => (
-  run(`reacthtmlpacktomchentwcom/build`, [`npm`, `start`], [process.stdout, process.stderr], {
+  run(process.env.DOCKER_BUILD_IMAGE_NAME, [`npm`, `start`], [process.stdout, process.stderr], {
     // https://docs.docker.com/engine/reference/api/docker_remote_api_v1.21/#create-a-container
     Tty: false,
     Env: [
@@ -22,7 +22,7 @@ export const runBuild = (dirpath) => (
     ],
     HostConfig: {
       Links: [
-        `meta_local-npm_1:local-npm`,
+        `${ process.env.DOCKER_LOCAL_NPM_CONTAINER_NAME }:local-npm`,
       ],
       VolumesFrom: [
         process.env.HOSTNAME, // http://stackoverflow.com/a/26979287
